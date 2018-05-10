77bde0380a725a7c54d7c267f7f5ce76b3b97176
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-10 10:01:12 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = 'open'
+  end
+  
+  def deposit(balance)
+    self.balance += balance
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
 
 end
